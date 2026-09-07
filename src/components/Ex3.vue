aed902ad25dfaaf126509acdebc2de014773d8a5
Ian Pong <105657662+peachoolongtee@users.noreply.github.com>
2026-09-07 10:26:25 +0800

    please 2

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -2,7 +2,7 @@
     import { ref } from 'vue'
 
     // Add code here
-    
+    const selectedOption = ref('username')
     
 </script>
 
@@ -10,14 +10,18 @@
     <h1>Please select your login option</h1>
     <!-- Add/modify code in the following to have a dropdown list 
         which lets the user to select either to login with username or email -->
-    <select>
-        <option selected>Username login</option>
-        <option>Email login</option>
+    <select v-model="selectedOption">
+        <option selected value='username'>Username login</option>
+        <option value='email'>Email login</option>
     </select>
     
-    <div>
+    <div v-if="selectedOption === 'username'">
         <label>Username</label>
         <input placeholder="Enter your username">
+    </div>
+    <div v-else>
+        <label>Email</label>
+        <input placeholder="Enter your email">
     </div>
 </template>
 
--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -14,7 +14,7 @@
     // implement methods below 
     // part 1
     function changeColor() {
-        if (blueBox.value) {
+        if (blueBox.value === true) {
             blueBox.value = false
             blueBtn.value = false
         } else {
@@ -42,19 +42,24 @@
     <!-- note: need to use single quotes '' for classes such as btn-primary which contains '-' signs
                 because '-' is a minus operator for (Vue) JavaScript  -->
     <div id="part1">
-        <div>
+        <div :id="id" :class="{'blueBox': blueBox, 'redBox': !blueBox}">
             div ID : {{id}} 
         </div>
         
-        <button type="button" v-on:click="changeColor">Change Color</button>
+        <button type="button" v-on:click="changeColor"
+        v-bind:class="{'btn': true, 'btn-primary': !redBtn, 'btn-danger': redBtn}">Change Color</button>
     </div>
 
     <div id="part2">
-        <div>
+        <div :style="{
+            color: activeColor
+        }">
             div ID : {{id}} 
         </div>
        
-        <button type="button" v-on:click="changeTextColor">Change Text Color</button> 
+        <button type="button"
+        v-bind:class="{'btn': true, 'btn-primary': !redBtn, 'btn-danger': redBtn}"
+        @click="changeTextColor">Change Text Color</button> 
     </div>
 
 </template>
